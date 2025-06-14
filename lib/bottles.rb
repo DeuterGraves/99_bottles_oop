5f1f682794bc5d32b827676eb704c5e3181a2519
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-14 15:54:09 +0100

    pedantic tdd: first test passing

---
+++ lib/bottles.rb
@@ -0,0 +1,6 @@
+class Bottles
+
+    def verse(count)
+        "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-=1} bottles of beer on the wall.\n"
+    end
+end
