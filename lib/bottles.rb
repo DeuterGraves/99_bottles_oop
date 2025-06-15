e162689f391528537d80e17b33e8546dec67e350
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-15 22:51:49 +0100

    now handles bottle/bottles for resulting number of bottlai

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,15 @@
 class Bottles
 
     def verse(count)
-        "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-=1} bottles of beer on the wall.\n"
+        "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-=1} #{bottle_plural_agreement(count)} of beer on the wall.\n"
     end
+
+    def bottle_plural_agreement(count)
+        if count == 1
+            "bottle"
+        else
+            "bottles"
+        end
+    end
+
 end
