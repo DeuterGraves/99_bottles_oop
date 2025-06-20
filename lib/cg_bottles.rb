7af52249172bb1fe91c64234a6675ace9fb6677c
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 22:29:47 +0100

    Listing 2.19 verses 99 and 98 message - isolates responsibilities

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -13,10 +13,8 @@ class CgBottles
   end
 
   def verses(_,_)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"+
+    verse(99) +
     "\n" +
-    "98 bottles of beer on the wall, 98 bottles of beer.\n" +
-    "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+    verse(98)
   end
 end
