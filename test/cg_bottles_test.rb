3ae469a71279fdf8cadf463a32a642dd3656963c
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:11:31 +0100

    second test written, now failing as expected

--- test/cg_bottles_test.rb
+++ test/cg_bottles_test.rb
@@ -9,4 +9,10 @@ class CgBottlesTest < Minitest::Test
     "Take one down and pass it around, 98 bottles of beer on the wall.\n"
     assert_equal expected, CgBottles.new.verse(99)
   end
+
+  def test_another_verse
+    expected = "3 bottles of beer on the wall, 3 bottles of beer.\n"+
+    "Take one down and pass it around, 2 bottles of beer on the wall.\n"
+    assert_equal expected, CgBottles.new.verse(3)
+  end
 end
