78025256895413f6004d9efc722174c82ebd7010
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:36:06 +0100

    Listing 2.13: Verse 1 code

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,6 +1,9 @@
 class CgBottles
   def verse(count)
-    if count == 2
+    case count
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
       "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
