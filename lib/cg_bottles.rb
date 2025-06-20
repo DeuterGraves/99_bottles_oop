31f928ec3cefa023cf0a1aaec47d1ec0ec644bc5
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:16:56 +0100

    Listing 2.8: Generalised Verse - this is similar to the route I took originally

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,11 +1,5 @@
 class CgBottles
   def verse(count)
-    if count == 99
-      n = 99
-    else
-      n = 3
-    end
-
-    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+    "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
   end
 end
