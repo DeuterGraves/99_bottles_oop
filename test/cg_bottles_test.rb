9cf39092bad30f9a44553496fbf87d05b70b4ea5
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 22:42:15 +0100

    New test added this test to assist in triangulating to identify opportunities for abstraction

--- test/cg_bottles_test.rb
+++ test/cg_bottles_test.rb
@@ -39,11 +39,23 @@ class CgBottlesTest < Minitest::Test
 
   def test_a_couple_verses
     expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"+
+    "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
     "\n" +
     "98 bottles of beer on the wall, 98 bottles of beer.\n" +
     "Take one down and pass it around, 97 bottles of beer on the wall.\n"
 
     assert_equal expected, CgBottles.new.verses(99, 98)
+  end
+
+  def test_a_few_verses
+    expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
+    "\n" +
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+    "Take one down and pass it around, no more bottles of beer on the wall.\n" +
+    "No more bottles of beer on the wall, no more bottles of beer.\n" +
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+    assert_equal expected, CgBottles.new.verses(2, 0)
   end
 end
