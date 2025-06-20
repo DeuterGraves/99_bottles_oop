baee866e8fa9c76cef3f8cc794d1309c5a198a1d
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:24:01 +0100

    Listing 2.11 Conditional with Embedded Logic

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,9 +1,5 @@
 class CgBottles
   def verse(count)
-    if count == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
-    end
+    "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottle#{'s' unless (count-1) == 1} of beer on the wall.\n"
   end
 end
