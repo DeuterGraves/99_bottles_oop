af2b6bb3561e7bb580aaa79a40a4404203cff11e
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 16:48:20 +0100

    test for a couple of verses added and as expected, failing

--- test/cg_bottles_test.rb
+++ test/cg_bottles_test.rb
@@ -36,4 +36,14 @@ class CgBottlesTest < Minitest::Test
 
     assert_equal expected, CgBottles.new.verse(0)
   end
+
+  def test_a_couple_verses
+    expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+    "Take one down and pass it around, 98 bottles of beer on the wall.\n"+
+    "\n"
+    "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+    "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+
+    assert_equal expected, CgBottles.new.verses(99, 98)
+  end
 end
