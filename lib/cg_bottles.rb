cd93d2b23a6a9fab8f9908d4511d489104ff75db
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 17:00:11 +0100

    Listing 2.17 Verses 99 98 Literal: boy isn't this a bit cheeky

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -11,4 +11,12 @@ class CgBottles
       "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
     end
   end
+
+  def verses(_,_)
+    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+    "Take one down and pass it around, 98 bottles of beer on the wall.\n"+
+    "\n" +
+    "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+    "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+  end
 end
