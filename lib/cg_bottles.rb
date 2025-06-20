a1a00c18ddfe60290c4a46e96ecc061f7dcc1338
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:15:43 +0100

    listing 2.7: sparce conditional

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,9 +1,11 @@
 class CgBottles
   def verse(count)
     if count == 99
-      "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+      n = 99
     else
-      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+      n = 3
     end
+
+    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
   end
 end
