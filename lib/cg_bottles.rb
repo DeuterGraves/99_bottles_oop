c5c8d39319f079792b2d3bedebd15bfbda989d3f
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:13:24 +0100

    following listing 2.6: conditional - now passes

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,5 +1,9 @@
 class CgBottles
-  def verse(_)
-    "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse(count)
+    if count == 99
+      "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+    else
+      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+    end
   end
 end
