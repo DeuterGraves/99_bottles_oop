d49db58efc7cc6f421c6fdb7b025f3e7fd4a58a3
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:55:39 +0100

    Listing 2.15: Verse 0 Code - adding a case for bottle count: 0

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,6 +1,8 @@
 class CgBottles
   def verse(count)
     case count
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n"
     when 2
