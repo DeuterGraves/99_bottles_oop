2b58384ac22281cefecdb103173fb678b3faf16d
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 16:59:31 +0100

    fixing the test cos I forgot a plus sign

--- test/cg_bottles_test.rb
+++ test/cg_bottles_test.rb
@@ -40,7 +40,7 @@ class CgBottlesTest < Minitest::Test
   def test_a_couple_verses
     expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
     "Take one down and pass it around, 98 bottles of beer on the wall.\n"+
-    "\n"
+    "\n" +
     "98 bottles of beer on the wall, 98 bottles of beer.\n" +
     "Take one down and pass it around, 97 bottles of beer on the wall.\n"
 
