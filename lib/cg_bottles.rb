d120981f4417c6aeb43c8e0e18102973e2d511c2
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:28:16 +0100

    Listing 2.11a Conditional with Embedded Logic and premature abstraction

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,5 +1,9 @@
 class CgBottles
   def verse(count)
-    "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottle#{'s' unless (count-1) == 1} of beer on the wall.\n"
+    "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} #{pluralize(count)} of beer on the wall.\n"
+  end
+
+  def pluralize(count)
+    "bottle#{'s' unless (count-1) == 1}"
   end
 end
