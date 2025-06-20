323a733bb8934fd87a2a17c49984086d628c8dce
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-06-20 15:30:11 +0100

    Returning to Listing 2.10 to proceed on Shameless Green Road

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -1,9 +1,9 @@
 class CgBottles
   def verse(count)
-    "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} #{pluralize(count)} of beer on the wall.\n"
-  end
-
-  def pluralize(count)
-    "bottle#{'s' unless (count-1) == 1}"
+    if count == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
+    end
   end
 end
