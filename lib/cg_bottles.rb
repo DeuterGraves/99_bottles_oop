9c043b8e43a316939d953c630ea71e917cc6f239
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-11-26 11:24:57 +0000

    replace  in else case with new method call

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -12,7 +12,7 @@ class CgBottles
     # when 7
     #   "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 1 six-pack of beer on the wall.\n"
     else
-      "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
+      "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} #{container} of beer on the wall.\n"
     end
   end
 
