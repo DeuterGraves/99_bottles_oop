140f141a99948f79bcba1d51d88e537a8d807b62
Caroline Graves <41622204+DeuterGraves@users.noreply.github.com>
2025-12-18 13:13:42 +0000

    delete obsolete cases - tests still pass!

--- lib/cg_bottles.rb
+++ lib/cg_bottles.rb
@@ -5,12 +5,6 @@ class CgBottles
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} #{container(count-1)} of beer on the wall.\n"
-    # when 6
-    #   "1 six-pack of beer on the wall, 1 six-pack of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n"
-    # when 7
-    #   "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 1 six-pack of beer on the wall.\n"
     else
       "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} #{container(count-1)} of beer on the wall.\n"
     end
